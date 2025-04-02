24d9a4679f7290920e68bc7eb6aa47b29b65acf9
JavidanAhmadliAze <axmadlijavidan@gmail.com>
2025-04-02 09:57:23 +0300

    test multiple inputs

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pytest
-@pytest.fixture
-def sample_data():
- return {"name": "Alice", "age": 30}
-def test_sample_data(sample_data):
- assert sample_data["name"] == "Alice"
- assert sample_data["age"] == 30
+
+
+def add(a: int, b: int):
+    return a + b
+
+
+@pytest.mark.parametrize("a, b, expected", [(2, 3, 5), (-1, 1, 0), (0, 0, 0)])
+def test_add_param(a, b, expected):
+ assert add(a, b) == expected
+
